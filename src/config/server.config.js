1f73409b316e9bc9f99d867c51226143a2de4c01
Natalia Petkevich <petkevich.natalli@gmail.com>
2018-08-29 14:13:25 +0300

    fix for heroky

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const mongoose = require('./mongoose.config');
 const cors = require('cors');
@@ -12,7 +12,7 @@ app
   // .disable('x-powered-by')
   .use(morgan('dev'))
   .use(cors())
-  .use(favicon(path.join(process.cwd(), './public', 'favicon.ico')))
+  // .use(favicon(path.join(process.cwd(), './public', 'favicon.ico')))
   .use(express.static(path.join(process.cwd(), publicDir)))
   .use(bodyParser.json({ type: 'text/plain' }))
   .use(bodyParser.urlencoded({ extended: false }))
